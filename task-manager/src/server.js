cd8b097d60912116742dfda6ab057077fd1c9f31
Matthew McGrath <rubywep@hotmail.com>
2022-04-01 11:40:22 -0700

    Add errorHandler middleware to all routes

--- task-manager/src/server.js
+++ task-manager/src/server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 require('./db/mongoose.js');
 const auth = require('./middleware/auth');
+const errorHandler = require('./middleware/errorHandler');
 const userRouter = require('./routers/userRouter');
 const taskRouter = require('./routers/taskRouter');
 
 const app = express();
+const port = process.env.PORT;
 
-const port = process.env.PORT || 5000;
-
-// app.use(auth);
 app.use(express.json());
 app.use('/users', userRouter);
 app.use('/tasks', taskRouter);
+
+// Use a custom error handler middleware to replace Express' default
+// It's important this comes after our routers!
+app.use(errorHandler);
 
 app.listen(5000, () => {
   console.log(`Server running on port ${port}!`);
